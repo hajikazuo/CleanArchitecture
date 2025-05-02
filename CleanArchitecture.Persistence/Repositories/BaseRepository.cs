6a23fe28b4f6b2ad667028767b8a34ecb3b57cd6
hajikazuo <hajikazuo@outlook.com>
2025-05-02 17:21:21 -0300

    Add project files.

---
+++ CleanArchitecture.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAll(CancellationToken cancellationToken)
+        {
+            return await _context.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public async Task<T> GetById(Guid id, CancellationToken cancellationToken)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public void Add(T entity)
+        {
+            entity.DateCreated = DateTimeOffset.UtcNow;
+            _context.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            entity.DateUpdated = DateTimeOffset.UtcNow;
+            _context.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            entity.DateDeleted = DateTimeOffset.UtcNow;
+            _context.Remove(entity);
+        }
+    }
+}
