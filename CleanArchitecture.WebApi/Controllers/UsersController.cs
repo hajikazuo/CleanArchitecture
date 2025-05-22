0253be6c5bd03b42c1d9b161bab63074bdd27e7b
hajikazuo <hajikazuo@outlook.com>
2025-05-22 14:43:20 -0300

    feat update and delete use cases

--- CleanArchitecture.WebApi/Controllers/UsersController.cs
+++ CleanArchitecture.WebApi/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
-﻿using CleanArchitecture.Application.UseCases.CreateUser;
+﻿using Azure.Core;
+using CleanArchitecture.Application.UseCases.CreateUser;
+using CleanArchitecture.Application.UseCases.DeleteUser;
 using CleanArchitecture.Application.UseCases.GetAllUsers;
+using CleanArchitecture.Application.UseCases.UpdateUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +32,32 @@ namespace CleanArchitecture.WebApi.Controllers
         public async Task<ActionResult<CreateUserResponse>> Create(CreateUserRequest request, CancellationToken cancellationToken)
         {
             var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UpdateUserResponse>> Update(Guid id, UpdateUserRequest request, CancellationToken cancellationToken)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DeleteUserResponse>> Delete(Guid? id, CancellationToken cancellationToken)
+        {
+            if (id is null)
+            {
+                return BadRequest();
+            }
+
+            var deleteUserRequest = new DeleteUserRequest(id.Value);
+
+            var response = await _mediator.Send(deleteUserRequest, cancellationToken);
             return Ok(response);
         }
     }
